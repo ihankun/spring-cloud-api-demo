eaf770fbc027b9a1cdc1f81a20e5f59fe4ab8bc3
hankun <ihankun@outlook.com>
2023-05-27 16:25:16 +0800

    hankun-引入logback

--- spring-cloud-api-demo-server/src/main/java/com/ihankun/demo/server/hello/service/impl/HelloServiceImpl.java
+++ spring-cloud-api-demo-server/src/main/java/com/ihankun/demo/server/hello/service/impl/HelloServiceImpl.java
@@ -4,12 +4,16 @@ import com.ihankun.demo.server.hello.service.HelloService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author hankun
+ */
 @Slf4j
 @Service
 public class HelloServiceImpl implements HelloService {
 
     @Override
     public String hello() {
+        log.info("hello api");
         return "hello api!";
     }
 }
